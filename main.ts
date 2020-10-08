e8597c057d0d406b1fae06234983333bf3e10905
Mariam-Abugamga <71400493+Mariam-Abugamga@users.noreply.github.com>
2020-10-08 13:18:52 -0400

    added a comment and the program

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+// Created By: Mariam Abugamga
+// Created On: oct, 8, 2020
+// this program fins the cost of a pizza using the diameter.
+game.splash("Pizza Order")
+let LABOUR_COST = 0.75
+let RENT = 1
+let MATERIALS = 0.5
+let GOODS_AND_SERVICES_TAX = 1.13
+let diameter_inch_of_pizza = game.askForNumber("Enter the diameter of pizza (inch)")
+let pizza = LABOUR_COST + RENT + MATERIALS * (diameter_inch_of_pizza + GOODS_AND_SERVICES_TAX)
+game.splash("The cost of the pizza is $" + Math.round(pizza * 100) / 100)
